f35badab21391c9e9549022df2e980589a85e60c
Bruno Tavares <27867141+bmtavares@users.noreply.github.com>
2020-09-15 16:45:48 +0100

    Project Init

---
+++ CountriesExplorer.Prism/CountriesExplorer.Prism/Views/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CountriesExplorer.Prism.Views
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CountriesExplorer.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CountriesExplorer.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
